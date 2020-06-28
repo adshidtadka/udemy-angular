a7b7711c7f834f93d7e6bba8a4335bede8b30343
adshidtadka <nextage900fx@gmail.com>
2020-06-28 15:53:41 +0900

    チャットの編集, 削除を実装する

--- angular-chat/src/app/app.component.ts
+++ angular-chat/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Comment } from './class/comment';
 import { User } from './class/user';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { Observable } from 'rxjs';
 
 const CURRENT_USER: User = new User(1, '立川めめめ');
 const ANOTHER_USER: User = new User(2, '戸川めめめ');
@@ -13,20 +12,46 @@ const ANOTHER_USER: User = new User(2, '戸川めめめ');
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  comments: Observable<any>;
+  comments: Comment[];
   commentsRef: AngularFireList<any>;
   currentUser = CURRENT_USER;
   content = '';
 
   constructor(private db: AngularFireDatabase) {
     this.commentsRef = db.list('comments');
-    this.comments = this.commentsRef.valueChanges();
+    this.commentsRef.snapshotChanges().subscribe((snapshots) => {
+      this.comments = snapshots.map((snapshot) => {
+        const values = snapshot.payload.val();
+        return new Comment({ key: snapshot.payload.key, ...values });
+      });
+    });
   }
 
   addComment(comment: string): void {
     if (comment) {
-      this.commentsRef.push(new Comment(this.currentUser, comment));
+      this.commentsRef.push(
+        new Comment({ user: this.currentUser, message: comment })
+      );
       this.content = '';
     }
+  }
+
+  toggleEditComment(index: number): void {
+    this.comments[index].isEdit = !this.comments[index].isEdit;
+  }
+
+  saveEditComment(index: number, key: string): void {
+    this.commentsRef
+      .update(key, {
+        message: this.comments[index].message,
+        date: this.comments[index].date,
+      })
+      .then(() => {
+        this.comments[index].isEdit = false;
+      });
+  }
+
+  deleteComment(key: string): void {
+    this.commentsRef.remove(key);
   }
 }
