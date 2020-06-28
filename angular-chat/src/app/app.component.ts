857d005aec34dcff413a775f3e373661f20dc645
adshidtadka <nextage900fx@gmail.com>
2020-06-28 13:25:24 +0900

    タイムラインを表示する

--- angular-chat/src/app/app.component.ts
+++ angular-chat/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  { name: '伊藤なんちゃら', message: 'お疲れ様です!' },
+  { name: '斎藤なんちゃら', message: 'この間の件ですが, どうなりましたか?' },
+  { name: '加藤なんちゃら', message: 'お疲れ様です!' },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
 }
