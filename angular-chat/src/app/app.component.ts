eb14891e98bf91421acbf4c718d5b504f18896f5
adshidtadka <nextage900fx@gmail.com>
2020-06-28 15:17:53 +0900

    angularとfirebaseを連携

--- angular-chat/src/app/app.component.ts
+++ angular-chat/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Comment } from './class/comment';
 import { User } from './class/user';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 const CURRENT_USER: User = new User(1, '立川めめめ');
 const ANOTHER_USER: User = new User(2, '戸川めめめ');
@@ -18,6 +20,18 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  item: Observable<any>;
   comments = COMMENTS;
   currentUser = CURRENT_USER;
+  content = '';
+
+  constructor(private db: AngularFireDatabase) {
+    this.item = db.object('item').valueChanges();
+  }
+
+  addComment(comment: string): void {
+    if (comment) {
+      this.comments.push(new Comment(this.currentUser, comment));
+    }
+  }
 }
--- angular-chat/src/app/app.module.ts
+++ angular-chat/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentDatePipe } from './pipe/comment-date.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CommentDatePipe],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
